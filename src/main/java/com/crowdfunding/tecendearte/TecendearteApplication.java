b9df3e30998157d4bf316b467f89aa92d6bc6bdb
gabysouzz <gabrielly.souza.lima@ccc.ufcg.edu.br>
2025-07-29 23:19:01 -0300

    Inicializa projeto Spring Boot com estrutura base

---
+++ src/main/java/com/crowdfunding/tecendearte/TecendearteApplication.java
@@ -0,0 +1,13 @@
+package com.crowdfunding.tecendearte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecendearteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecendearteApplication.class, args);
+	}
+
+}
